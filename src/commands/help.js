0d2cee52e80a11d278041ce3a008eb8d235a3812
Eetu Niittymaki <eetu.niittymaki@amabit.fi>
2026-02-16 13:27:12 +0200

    Change how help screen info is displayed

--- src/commands/help.js
+++ src/commands/help.js
@@ -7,7 +7,7 @@ const blue = chalk.blueBright
 
 export const help = () => {
     console.log(`${white('eetool [CMD]')}
-    ${green('--help')}\t${yellow('Get list of available commands')}
+    ${green('--help')}\t${white('eetool --help')}\n\t\t${yellow('Get list of available commands')}
     ${green('--tabs')}\t${white('eetool --tabs <file>')}\n\t\t${yellow('Converts spaces to tabs in given file:')}
     ${green('--spaces')}\t${white('eetool --spaces <file>')}\n\t\t${yellow('Converts tabs to spaces in given file:')}
     ${green('--password')}\t${white('eetool --password {length} {flags}')}\n\t\t${yellow('Randomnly generates a password with a default length of 24:')}\n\t\t${blue('Optional flags: -c -> copy to clipboard, -s -> use special characters')}
