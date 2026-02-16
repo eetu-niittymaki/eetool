588d287eff0b6c1fdf4f1e5e09eea2b93df7005c
Eetu Niittymaki <eetu.niittymaki@amabit.fi>
2026-02-16 11:57:46 +0200

    Change how --help screen info is displayed

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,11 +1,16 @@
 import chalk from 'chalk'
 
+const green = chalk.greenBright
+const yellow = chalk.yellowBright
+const white = chalk.whiteBright
+const blue = chalk.blueBright
+
 export const help = () => {
-    console.log(`${chalk.whiteBright('eetool [CMD]')}
-    ${chalk.greenBright('--help')}\tGet list of available commands
-    ${chalk.greenBright('--tabs')}\tConverts spaces to tabs in given file:\n\t\teetool --tabs <file>
-    ${chalk.greenBright('--spaces')}\tConverts tabs to spaces in given file: \n\t\teetool --spaces <file> 
-    ${chalk.greenBright('--password')}\tRandomnly generates a password with a default length of 24: \n\t\teetool --password <length> {flags}. \n\t\tOptional flags: -c -> copy to clipboard, -s -> use special characters
-    ${chalk.greenBright('--image')}\tReformat images, optionally also resize them: \n\t\teetool --image <original file, i.e img.jpg> <output file and type, i.e img.png> {optional width} {optional height}.
+    console.log(`${white('eetool [CMD]')}
+    ${green('--help')}\t${yellow('Get list of available commands')}
+    ${green('--tabs')}\t${yellow('Converts spaces to tabs in given file:')}\n\t\t${white('eetool --tabs <file>')}
+    ${green('--spaces')}\t${yellow('Converts tabs to spaces in given file:')}\n\t\t${white('eetool --spaces <file>')}
+    ${green('--password')}\t${yellow('Randomnly generates a password with a default length of 24:')}\n\t\t${white('eetool --password (length) {flags}')}\n\t\t${blue('Optional flags: -c -> copy to clipboard, -s -> use special characters')}
+    ${green('--image')}\t${yellow('Reformat images, optionally also resize them:')} \n\t\t${white('eetool --image <original file, i.e img.jpg> <output file and type, i.e img.png> {optional width} {optional height}')}
     `)
 }
