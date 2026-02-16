a3114dfed347da8c679f44d7739f3601a01fdfed
Eetu Niittymaki <eetu.niittymaki@amabit.fi>
2026-02-16 13:26:07 +0200

    Change how help screen info is displayed

--- README.md
+++ README.md
@@ -25,8 +25,8 @@ At the moment only allows you to change tabs to spaces and vice versa, randomly 
 * eetool --help, For list of available commands
 * eetool --tabs, Converts spaces to tabs in given file. eetool --tabs (file)
 * eetool --spaces, Converts tabs to spaces in given file. eetool --spaces (file)
-* eetool --password, Randomnly generates a password with a default length of 24. eetool --password (length) {flags}. Optional flags: -c -> Copy to clipboard, -s -> use special characters. 
-* eetool --image, Reformat images, optionally also resize them. eetool --image (original file, i.e img.jpg ) (output file and type, i.e img.png) {optional width} {optional height}.
+* eetool --password, Randomnly generates a password with a default length of 24. eetool --password {length} {flags}. Optional flags: -c -> Copy to clipboard, -s -> use special characters. 
+* eetool --image, Reformat images, optionally also resize them. eetool --image (infile, i.e img.jpg ) (outfile, i.e img.png) {width} {height}.
 
 ## Acknowledgments
 
--- src/commands/help.js
+++ src/commands/help.js
@@ -8,9 +8,9 @@ const blue = chalk.blueBright
 export const help = () => {
     console.log(`${white('eetool [CMD]')}
     ${green('--help')}\t${yellow('Get list of available commands')}
-    ${green('--tabs')}\t${yellow('Converts spaces to tabs in given file:')}\n\t\t${white('eetool --tabs <file>')}
-    ${green('--spaces')}\t${yellow('Converts tabs to spaces in given file:')}\n\t\t${white('eetool --spaces <file>')}
-    ${green('--password')}\t${yellow('Randomnly generates a password with a default length of 24:')}\n\t\t${white('eetool --password (length) {flags}')}\n\t\t${blue('Optional flags: -c -> copy to clipboard, -s -> use special characters')}
-    ${green('--image')}\t${yellow('Reformat images, optionally also resize them:')} \n\t\t${white('eetool --image <original file, i.e img.jpg> <output file and type, i.e img.png> {optional width} {optional height}')}
+    ${green('--tabs')}\t${white('eetool --tabs <file>')}\n\t\t${yellow('Converts spaces to tabs in given file:')}
+    ${green('--spaces')}\t${white('eetool --spaces <file>')}\n\t\t${yellow('Converts tabs to spaces in given file:')}
+    ${green('--password')}\t${white('eetool --password {length} {flags}')}\n\t\t${yellow('Randomnly generates a password with a default length of 24:')}\n\t\t${blue('Optional flags: -c -> copy to clipboard, -s -> use special characters')}
+    ${green('--image')}\t${white('eetool --image <infile, i.e img.jpg> <outfile, i.e img.png> {width} {height}')}\n\t\t${yellow('Reformat images, optionally also resize them if width and height given')}
     `)
 }
